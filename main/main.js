02774259f57f5571bb61b95d227852bbb34f3122
huangzhi <741004077@qq.com>
2019-05-31 21:37:35 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(num) {
+    var str_num = num.toString();
+    var len = str_num.length;
+    var res="";
+    var sig = [' ','...', '._.', '|.|','..|','._|','|..','|_.','|_|'];
+    var arr = [[2,3,8],[1,4,4],[2,5,7],[2,5,5],[1,8,4],[2,7,5],[2,7,8],[2,4,4],[2,8,8],[2,8,4]];
+    for(var i=0;i<3*len;i++){
+        var row = 
+        res = res + sig[arr[parseInt(str_num[i%len])][Number(i>=len)+Number(i>=2*len)+Number(i>=3*len)]];
+        if(i%len==(len-1)&&i!=(len-1)){    res = res + '\n';}
+        else{    res = res +' ';}
+        
+    }
+    console.log(res);
+    return res;
 };
